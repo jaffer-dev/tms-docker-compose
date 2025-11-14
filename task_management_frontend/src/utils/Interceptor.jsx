b6ceaade351bdd319209a1daa9064b7c7130f736
jaffer-dev <jaffer.zaidi@kamelpay.com>
2025-11-14 21:43:46 +0500

    Update BASE_URL in Interceptor.jsx

--- task_management_frontend/src/utils/Interceptor.jsx
+++ task_management_frontend/src/utils/Interceptor.jsx
@@ -5,7 +5,7 @@ import { TOKEN } from "./Constants";
 
 // const BASE_URL = 'https://backend-production-c45e.up.railway.app/api'
 // const BASE_URL = 'http://192.168.105.189:5000/api'
-const BASE_URL = 'https://10.10.5.108:6000/api'
+const BASE_URL = 'https://10.10.5.108:8080/api'
 // const BASE_URL = 'http://192.168.0.111:5000/api'
 // const BASE_URL = 'https://6449e930925b.ngrok-free.app/api'
 
